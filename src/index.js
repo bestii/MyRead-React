2cbeabf3e470c13a390c649d526b71cda22581c7
Bestin John <bjohn@mcfadyen.com>
2018-08-25 22:01:03 +0530

    Setup Routing
    # Created BookList Component
    # Created Search Component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRoute} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <BrowserRoute>
+    <BrowserRouter>
         <App />
-    </BrowserRoute>, 
+    </BrowserRouter>, 
     document.getElementById('root'));
 registerServiceWorker();
