27ecc691260b53a278f0dfaad0649f9260125191
Bestin John <bjohn@mcfadyen.com>
2018-08-26 00:08:50 +0530

    Removed static content and setup dynamic data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import * as BooksAPI from './BooksAPI';
+import * as BooksAPI from './BooksAPI';
 import { Route } from 'react-router-dom';
 import './App.css';
 import Search from './Search';
@@ -8,19 +8,22 @@ import BookList from './BookList';
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
+  }
+
+  componentDidMount(){
+    BooksAPI.getAll().then((books)=>{
+      this.setState({books});
+    });
   }
 
   render() {
+    const {books} = this.state;
     return (
       <div className="app">
-        <Route exact path="/" component={BookList} />
+        <Route exact path="/" render={()=>(
+          <BookList books={books}/>
+        )}/>
         <Route path="/search" component={Search} />
       </div>
     )
