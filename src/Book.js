8286fbb17e31f7193fae9bb0904e347fb8c0fedf
Bestin John <bestinjohn1993@gmail.com>
2018-08-26 13:32:07 +0530

    Created SelectShelf Component

--- src/Book.js
+++ src/Book.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types'
+import SelectShelf from './SelectShelf';
 
 function Book(props) {
     const { book } = props;
@@ -10,16 +11,9 @@ function Book(props) {
             <div className="book">
                 <div className="book-top">
                     <div className="book-cover"
-                        style={{ width: 128, height: 193, backgroundImage: `url(${coverImg})` }}></div>
-                    <div className="book-shelf-changer">
-                        <select>
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                        </select>
+                        style={{ width: 128, height: 193, backgroundImage: `url(${coverImg})` }}>
                     </div>
+                    <SelectShelf/>
                 </div>
                 <div className="book-title">{title}</div>
                     { 
